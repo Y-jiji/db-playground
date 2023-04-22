6821352ce6e979314572bac11200803ee066e80a
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-04-22 13:46:39 +0800

    Refactor: parameter in bench test

--- db-core/src/rw_control/kv_sparkle/bench.rs
+++ db-core/src/rw_control/kv_sparkle/bench.rs
@@ -1,4 +1,6 @@
 const NULL_WRITE: bool = false;
+const RD_LATENCY: u64  = 5;
+const WR_LATENCY: u64  = 5;
 
 #[test]
 fn run_u64_unif() {
@@ -9,7 +11,7 @@ fn run_u64_unif() {
     use crate::tx_service::m_thread::*;
     use super::*;
     // durablility control, null control
-    let dur = Null::<U64Tup, KVSparkleTx<U64Tup, U64Txn>>::new(5, 5, NULL_WRITE);
+    let dur = Null::<U64Tup, KVSparkleTx<U64Tup, U64Txn>>::new(RD_LATENCY, WR_LATENCY, NULL_WRITE);
     // concurrency control, kv sparkle in this module
     let con = super::KVSparkle::<U64Txn, U64Tup>::new();
     // service, multi-thread service
@@ -26,7 +28,7 @@ fn run_revm_10key() {
     use crate::tx_service::m_thread::*;
     use super::*;
     // durability control, null control
-    let dur = Null::<EVMU256Tup, KVSparkleTx<EVMU256Tup, REVMInterpTxn>>::new(5, 5, NULL_WRITE);
+    let dur = Null::<EVMU256Tup, KVSparkleTx<EVMU256Tup, REVMInterpTxn>>::new(RD_LATENCY, WR_LATENCY, NULL_WRITE);
     // concurrency control, kv sparkle in this module
     let con = super::KVSparkle::<REVMInterpTxn, EVMU256Tup>::new();
     // service, multi-thread service
--- db-core/src/rw_control/null/bench.rs
+++ db-core/src/rw_control/null/bench.rs
@@ -1,4 +1,6 @@
 const NULL_WRITE: bool = false;
+const RD_LATENCY: u64  = 5;
+const WR_LATENCY: u64  = 5;
 
 #[test]
 fn run_u64_unif() {
@@ -9,7 +11,7 @@ fn run_u64_unif() {
     use db_test::core_workload::int::unif::*;
     // parameter, if set to non-zero, testing will be super slow!
     // durablility control, null control
-    let dur = Null::<U64Tup, U64Txn>::new(5, 5, NULL_WRITE);
+    let dur = Null::<U64Tup, U64Txn>::new(RD_LATENCY, WR_LATENCY, NULL_WRITE);
     // concurrency control, kv sparkle in this module
     let con = super::Null::<U64Tup, U64Txn>::new();
     // service, multi-thread service
@@ -26,7 +28,7 @@ fn run_revm_10key() {
     use crate::tx_service::m_thread::*;
     use db_test::core_workload::eth::revm_interp::*;
     // durability control, null control
-    let dur = Null::<EVMU256Tup, REVMInterpTxn>::new(5, 5, NULL_WRITE);
+    let dur = Null::<EVMU256Tup, REVMInterpTxn>::new(RD_LATENCY, WR_LATENCY, NULL_WRITE);
     // concurrency control, kv sparkle in this module
     let con = super::Null::<EVMU256Tup, REVMInterpTxn>::new();
     // service, multi-thread service
